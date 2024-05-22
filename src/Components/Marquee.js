48c69c9240c0b54e60106e178e378a2a592b1205
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-05-22 08:58:59 -0500

    lista de mensajes

--- src/Components/Marquee.js
+++ src/Components/Marquee.js
@@ -1,35 +1,57 @@
-// src/Marquee.js
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Marquee.css'; // Asegúrate de crear este archivo para los estilos
 
 function Marquee() {
-
   const marqueeRef = useRef(null);
+  const [marqueeText, setMarqueeText] = useState('');
+  const [frases, setFrases] = useState([]);
+  const [currentFraseIndex, setCurrentFraseIndex] = useState(0);
+  const velocidad = 5; // Aumenta este valor para incrementar la velocidad
 
   useEffect(() => {
     const marqueeElement = marqueeRef.current;
-    let startPosition = marqueeElement.offsetWidth;
-    
+    let startPosition = window.innerWidth;
+    let requestId;
+
     const animate = () => {
-      startPosition--;
+      startPosition -= velocidad;
       if (startPosition < -marqueeElement.offsetWidth) {
         startPosition = window.innerWidth;
+        // Cambiar el mensaje al siguiente
+        setCurrentFraseIndex((prevIndex) => (prevIndex + 1) % frases.length);
       }
       marqueeElement.style.left = startPosition + 'px';
-      requestAnimationFrame(animate);
+      requestId = requestAnimationFrame(animate);
     };
 
-    
-
     animate();
-  }, []);
+
+    // Cargar el archivo JSON desde la carpeta `public`
+    fetch('/phrases.json')
+      .then(response => response.json())
+      .then(data => {
+        setFrases(data.frases);
+        // Seleccionar la primera frase
+        setCurrentFraseIndex(0);
+      })
+      .catch(error => {
+        console.error('Error al cargar el archivo JSON:', error);
+      });
+
+    return () => cancelAnimationFrame(requestId);
+  }, [frases.length]);
+
+  useEffect(() => {
+    if (frases.length > 0) {
+      setMarqueeText(frases[currentFraseIndex]);
+    }
+  }, [currentFraseIndex, frases]);
 
   return (
     <div className="marquee-container">
       <div className="marquee" ref={marqueeRef}>
-        Esta es una prueba chiquita
+        {marqueeText}
       </div>
-      
     </div>
   );
 }
